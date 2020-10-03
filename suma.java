23d3de1825753ac1a16966d1c4af9f11ea59e906
rohitsandhu <xxmdluffxx@gmail.com>
2020-10-03 15:05:07 +0200

    Fitxer suma modificat(angles);
    sdklfj slkfjsldkfjs
    dsf sdfsdfsdfsdf

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer numero: ");
+System.out.println("Write the first number: ");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero: ");
+System.out.println("Write the second number: ");
 numero2 = reader.nextInt();
 
