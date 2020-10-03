e426a25cfdfb63e1f897e014efccc3235bd04ece
rohitsandhu <xxmdluffxx@gmail.com>
2020-10-03 13:54:49 +0200

    Commit del fitxer suma.java
    jfdasklfj klsjfklsdjfskldjfklsdfj

---
+++ suma.java
@@ -0,0 +1,10 @@
+
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("Introduce el primer numero: ");
+numero1 = reader.nextInt();
+
+System.out.println("Introduce el segundo numero: ");
+numero2 = reader.nextInt();
+
